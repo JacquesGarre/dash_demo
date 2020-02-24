0aac93b22bd03cbd63e26f58dc73ab0d35958b82
Jaks <jcqs.gr@gmail.com>
2020-02-24 17:11:34 +0100

    Ajout graph des années de naissance

--- app.py
+++ app.py
@@ -1,31 +1,64 @@
-# -*- coding: utf-8 -*-
+# # -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import os
+
+
+
+# Read csv
+dir_path = os.path.dirname(os.path.realpath(__file__))
+csv = os.path.join(dir_path, "liste_apprenants-devdataia.csv")
+data = pd.read_csv(csv, encoding="windows-1252", sep=";") 
+print(data)
+print(data['Prénom'])
+
+
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash '),
+    html.H1(children='Promotion Simplon'),
+    html.H3(children='École Microsoft Dev Data-IA'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
+        Un exemple d'utilisation du framework Dash
     '''),
 
     dcc.Graph(
         id='example-graph',
         figure={
             'data': [
-                {'x': ["Ulysse", 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'Nancy'},
-                {'x': ["Ulysse", 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Metz'},
+                {'x': data['Prénom'], 'y': data['Année de Naissance'].fillna(0), 'type': 'bar', 'name': 'Nancy'},
+
             ],
             'layout': {
-                'title': 'Visualisation de données'
+                'title': 'Années de naissance de la promo',
+                'yaxis': {
+                    'range': [1950,2005]
+                }
             }
         }
     )
+
+    # dcc.Graph(
+    #     id='example-graph-2',
+    #     figure={
+    #         'data': [
+    #             {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'Nancy'},
+
+    #         ],
+    #         'layout': {
+    #             'title': 'Année de naissance'
+    #         }
+    #     }
+    # )
+
+
 ])
 
 if __name__ == '__main__':
