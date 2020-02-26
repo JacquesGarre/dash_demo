371653ea1d179cdc93748149fc3ed5fce271ff96
tflk <thibaut.fiolka@hotmail.fr>
2020-02-26 09:57:14 +0100

    Ajout du 2nd graphe

--- app.py
+++ app.py
@@ -1,30 +1,54 @@
-# -*- coding: utf-8 -*-
+# # -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import os
+import plotly.express as px
+
+
+# Read csv
+dir_path = os.path.dirname(os.path.realpath(__file__))
+csv = os.path.join(dir_path, "liste_apprenants-devdataia.csv")
+data = pd.read_csv(csv, encoding="windows-1252", sep=";") 
+#print(data)
+#print(data['Prénom'])
+data1 = data.groupby(data['Couleur T-Shirt'])['Couleur T-Shirt'].count()
+fig = px.pie(data, values= data1, names= ['noir', 'bleu', 'blanc'], title= 'Couleur de T-Shirt de la promo')
+
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash '),
+    html.H1(children='Promotion Simplon'),
+    html.H3(children='École Microsoft Dev Data-IA'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
+        Un exemple d'utilisation du framework Dash
     '''),
 
     dcc.Graph(
         id='example-graph',
         figure={
             'data': [
-                {'x': ["Ulysse", 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'Nancy'},
-                {'x': ["Ulysse", 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Metz'},
+                {'x': data['Prénom'], 'y': data['Année de Naissance'].fillna(0), 'type': 'bar', 'name': 'Nancy'},
+
             ],
             'layout': {
-                'title': 'Visualisation de données'
+                'title': 'Années de naissance de la promo',
+                'yaxis': {
+                    'range': [1950,2005]
+                }
             }
         }
+    ),
+
+    dcc.Graph(
+        id='example-graph-2',
+        figure= fig
     )
 ])
 
